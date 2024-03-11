7acc8388a6e462e110c56243cc2b964182028511
amank-sdm <aman.kumar@smartdatainc.net>
2024-03-12 00:12:17 +0530

    initial commit

---
+++ src/main/java/dev/com/productservice11mar/Productservice11marApplication.java
@@ -0,0 +1,13 @@
+package dev.com.productservice11mar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Productservice11marApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Productservice11marApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/com/productservice11mar/Productservice11marApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.com.productservice11mar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Productservice11marApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
